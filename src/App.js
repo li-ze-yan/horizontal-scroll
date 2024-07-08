02994825d1d7ef2dcec65c7e2eaadc631ed7cb08
tkx.cool <tkx.cool@gmail.com>
2024-07-09 03:46:04 +0800

    feat: add smooth scroll with lenis

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import HorizontalScroll from "./Components/HorizontalScroll/HorizontalScroll";
 import Section from "./Components/Section/Section";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <Section title="Light rail photography" />
-            <HorizontalScroll />
-            <Section title="End of the gallery" />
-        </div>
-    );
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+  return (
+    <div className="App">
+      <Section title="Light rail photography" />
+      <HorizontalScroll />
+      <Section title="End of the gallery" />
+    </div>
+  );
 }
 
 export default App;
